715aae1eca1e1239eaaaeeb0d7bb6f3f1ec594f0
Christiano0407 <placebo0704@gmail.com>
2022-03-24 10:00:24 -0600

    animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 console.group("App Travel Web");
+//**  ==== Animation GSAP == Variables == ========= */
+let tl = gsap.timeline((paused: true, reserved: true));
+
+const bookNow = document.querySelector(".book");
+const cancel = document.querySelector(".cancel");
+
+//** === Animations  === */
+bookNow.addEventListener("click", (e) => {
+ tl.play();
+});
+
+cancel.addEventListener("click", (e) => {
+ tl.reverse(); 
+})
+
 
 console.groupEnd();
