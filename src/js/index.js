d9acb1c31ca29ecebc8750a7c810b41381235002
Christiano0407 <placebo0704@gmail.com>
2022-03-25 08:00:51 -0600

    final card transitions

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,63 @@
 console.group("App Travel Web");
 //**  ==== Animation GSAP == Variables == ========= */
-let tl = gsap.timeline((paused: true, reserved: true));
+let t1 = gsap.timeline({ paused: true, reserved: true });
 
 const bookNow = document.querySelector(".book");
 const cancel = document.querySelector(".cancel");
 
-//** === Animations  === */
 bookNow.addEventListener("click", (e) => {
- tl.play();
+  t1.play();
 });
 
 cancel.addEventListener("click", (e) => {
- tl.reverse(); 
-})
+  t1.reverse();
+});
 
+t1.to(
+  ".body",
+  {
+    ease: "power1.inOut",
+    marginTop: 0,
+  },
+  0
+);
 
-console.groupEnd();
+t1.to(
+  ".header button",
+  {
+    ease: "power1.inOut",
+    opacity: 0,
+    scale: 0,
+  },
+  0
+);
+
+t1.to(
+  ".book",
+  {
+    ease: "power1.inOut",
+    opacity: 0,
+  },
+  0
+);
+
+t1.to(
+  ".cancel",
+  {
+    ease: "power1.inOut",
+    right: 20,
+    opacity: 1,
+  },
+  0
+);
+
+t1.to(
+  ".booking-container",
+  {
+    ease: "power1.inOut",
+    marginTop: -210,
+    zIndex: 10,
+    height: 500,
+  },
+  0
+);
